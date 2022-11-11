b0508f3fcc0f340c3d47029d9cd41f59f6ea6365
LongYorke <1269217124@qq.com>
2022-11-11 16:10:40 +0800

    feat: 登录路由测试

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '@/styles/index.scss'
+// 注意导入使用的是单引号
 
 createApp(App).use(store).use(router).mount('#app')
