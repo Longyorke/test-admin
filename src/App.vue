d58c47f8fd41507510684ecfb729edfa65b42311
LongYorke <1269217124@qq.com>
2022-11-11 11:17:40 +0800

    feat: 添加element-plus组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <nav>
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
+//element-plus插件
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  //element-plus插件
+  configureWebpack: config => {
+    config.plugins.push(AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }));
+    config.plugins.push(Components({
+      resolvers: [ElementPlusResolver()],
+    }));
+  }
 })
+
+
