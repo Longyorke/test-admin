45e613256f0617233a38ba7f625abb1679ae7a47
LongYorke <1269217124@qq.com>
2022-11-12 10:57:35 +0800

    feat: 添加表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,27 +2,27 @@
   <div class="login-container">
     <!-- 从element-plus拿到表单代码 -->
     <!-- 动态绑定属性model的内容为setup中设置的form变量 -->
-    <el-form :model="form" class="login-form">
+    <el-form ref="ruleFormRef" :model="form" class="login-form" :rules="rules">
       <div class="title-container">
         <h3 class="title">用户登录</h3>
       </div>
       <!-- 表单中每个表单输入项目 -->
-      <el-form-item>
+      <el-form-item prop='user_name'>
         <!-- icon图标：用户 -->
         <el-icon :size="20" class="svg-container">
           <User />
         </el-icon>
         <!-- 表单输入 -->
-        <el-input v-model="form.name" />
+        <el-input v-model="form.user_name" />
       </el-form-item>
-      <el-form-item>
+      <el-form-item prop='password'>
         <!-- icon图标：密码锁 -->
         <el-icon :size="20" class="svg-container">
           <Lock />
         </el-icon>
         <el-input v-model="form.password" />
       </el-form-item>
-      <el-button type="primary" class="login-button">登录</el-button>
+      <el-button type="primary" class="login-button" @click="submitForm()">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -30,12 +30,38 @@
 <script setup>
 import { ref } from 'vue' // 从vue导入ref
 import { User, Lock } from '@element-plus/icons-vue' // 从element-plus引入编写图标
+
 // import { reactive } from "vue";
-// const user = reactive({ user_name: 'LongYorke', password: '123456', })
+// const user = reactive({ user_user_name: 'LongYorke', password: '123456', })
 const form = ref({ // vue3.0 ref只能创建基础类型vue3.2中使用ref创建对象底层实际上也会reactive进行实现
-  name: '',
+  user_name: '',
   password: ''
 })
+
+// 表单验证
+const rules = ref({
+  user_name: [
+    { required: true, message: 'Please input Activity user name', trigger: 'blur' },
+    { min: 3, max: 15, message: 'Length should be 3 to 15', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: 'Please input Activity user name', trigger: 'blur' },
+    { min: 3, max: 15, message: 'Length should be 3 to 15', trigger: 'blur' }
+  ]
+})
+
+const ruleFormRef = ref(null) // 声明一个变量用于缓存 ref 属性涉及Dom 元素的获取
+// 提交触发统一校验异步函数
+const submitForm = async () => {
+  await ruleFormRef.value.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
+  })
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -74,7 +100,7 @@ $cursor: #fff;
         background: transparent;
         box-shadow: 0 0 0 0;
         border: 0px;
-        -webkit-appearance: none;
+        // -webkit-appearance: none;
         border-radius: 0px;
         padding: 12px 5px 12px 15px;
         color: $light_gray;
